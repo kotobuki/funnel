b8ffa258c95810f2a7313ebd3e5dea5e643fccb6
kotobuki <kotobuki@f8328ec6-752f-0410-b566-9d1b0944c958>
2007-06-04 08:03:33 +0000

    extracted GainerIO class: from: serial_port_test.rb to: gainer_io.rb
    fixed bugs about receive buffer handling
    受信バッファの取り扱いに関するバグを修正

--- trunk/sketchbook/serial_port_test.rb
+++ trunk/sketchbook/serial_port_test.rb
@@ -1,139 +1,50 @@
 #!/usr/bin/env ruby
 
-require "serial_port"
+require "gainer_io"
 
-class GainerIO < Funnel::SerialPort
-  @receiver
-  @dispatcher
-  @quitRequested = false
-  @commands = []
+devices = []
 
-  def talk(command, reply_length)
-    write(command + '*')
-
-    return nil if (reply_length < 1)
-
-    10.times do
-      break if (bytes_available == reply_length)
-      sleep(0.01)
-    end
-
-    if (bytes_available >= reply_length) then read(reply_length)
-    else return nil
-    end
-  end
-
-  def reboot
-    reply = talk("Q", 2)
-    sleep(0.1)
-    return reply
-  end
-
-  def getVersion
-    talk("?", 10)
-  end
-
-  def turnOnLED
-    talk("h", 2)
-  end
-
-  def turnOffLED
-    talk("l", 2)
-  end
-
-  def setConfiguration(configuration)
-    reply = talk("KONFIGURATION_#{configuration}", 16)
-    sleep(0.1)
-    return reply
-  end
-
-  def onEvent(i)
-    puts "event: #{i}"
-  end
-
-  def beginAnalogInput
-    talk("i", 0)
-  end
-
-  def endAnalogInput
-    talk("E", 0)
-  end
-
-  def startPolling
-    received = ''
-    commands = []
-
-    @receiver = Thread.new do
-      while !@quitRequested
-        sleep(0.01) if (bytes_available < 1)
-        received << read(bytes_available)
-
-        count = received.scan(/\*/).length
-        commands = received.split(/\*/)
-
-        @commands = []
-        count.times do |i|
-          @commands << commands.at(i) unless (commands.at(i) == nil)
-        end
-
-        dispatchEvents
-        received = commands.at(count) unless (commands.at(count) == nil)  # incomplete command
-      end
-    end
-  end
-
-  def dispatchEvents
-    return if (@commands == nil)
-
-    @commands.each do |command|
-      case command[0]
-      when ?i
-        values = command.unpack('xa2a2a2a2')
-#        puts "ain: #{values.at(0).hex}, #{values.at(1).hex}, #{values.at(2).hex}, #{values.at(3).hex}"  # ain 0..3
-      when ?h
-        puts "led: on"
-      when ?l
-        puts "led: off"
-      when ?N
-        puts "sw: on"
-      when ?F
-        puts "sw: off"
-      else
-        puts "unknown!"
-      end
-    end
-  end
-
-  def finishPolling
-    @quitRequested = true
-    @receiver.join(1)
-  end
+Dir.foreach("/dev") do | deviceName |
+  devices.push(deviceName) if (deviceName.index("cu.usbserial") == 0)
 end
 
-gio = GainerIO.new('/dev/cu.usbserial-A30009cF', 38400)
+if (devices.size < 1) then
+  raise "Can't find any I/O modules..."
+end
+
+gio = GainerIO.new('/dev/' + devices.at(0), 38400)
 
 puts gio.reboot
 puts gio.getVersion
 puts gio.setConfiguration(1)
 
-3.times do
-  gio.turnOnLED
-  sleep(0.2)
-  gio.turnOffLED
-  sleep(0.2)
-end
-
 gio.beginAnalogInput
 gio.startPolling
 
-sleep(5)
+1.times do
+  gio.turnOnLED
+  sleep(0.1)
+  gio.turnOffLED
+  sleep(0.1)
+end
 
-#3.times do
-#  gio.turnOnLED
-#  sleep(0.5)
-#  gio.turnOffLED
-#  sleep(0.5)
-#end
+sleep(2)
 
-gio.endAnalogInput
+3.times do
+  gio.turnOnLED
+  sleep(0.1)
+  gio.turnOffLED
+  sleep(0.1)
+end
+
+sleep(2)
+
+1.times do
+  gio.turnOnLED
+  sleep(0.1)
+  gio.turnOffLED
+  sleep(0.1)
+end
+
 gio.finishPolling
+gio.endAnalogInput
