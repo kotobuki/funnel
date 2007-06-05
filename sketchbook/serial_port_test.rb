a40db38d0d8be95260d889926ad91ec4ad4a3d4b
Shigeru Kobayashi <kotobuki@users.noreply.github.com>
2007-06-05 07:35:07 +0000

    implemented experimental event handler 実験的なイベントハンドラを実装した

--- sketchbook/serial_port_test.rb
+++ sketchbook/serial_port_test.rb
@@ -14,12 +14,24 @@ end
 
 gio = GainerIO.new('/dev/' + devices.at(0), 38400)
 
+def onEvent(type, value)
+  if (type == GainerIO::AIN_EVENT) then
+#    p value
+  else
+    puts "#{type}: #{value}"
+  end
+end
+
+gio.onEvent = method(:onEvent)
+
 puts gio.reboot
 puts gio.getVersion
 puts gio.setConfiguration(1)
 
 gio.beginAnalogInput
 gio.startPolling
+
+sleep(0.1)
 
 1.times do
   gio.turnOnLED
