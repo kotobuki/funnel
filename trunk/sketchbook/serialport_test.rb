8b42d7ba28ae30b2ec168aab719ab370de43b086
kotobuki <kotobuki@f8328ec6-752f-0410-b566-9d1b0944c958>
2007-07-08 03:11:02 +0000

    * fixed bugs about fps display

--- trunk/sketchbook/serialport_test.rb
+++ trunk/sketchbook/serialport_test.rb
@@ -2,6 +2,7 @@
 
 require "serial_port"
 
+#@sp = Funnel::SerialPort.new('COM3', 38400)
 @sp = Funnel::SerialPort.new('/dev/cu.usbserial-A30009cb', 38400)
 
 def talk(command, length)
@@ -18,14 +19,16 @@ p talk('?*', 10)
 p talk('KONFIGURATION_1*', 16)
 sleep(0.1)
 
-# 2 bytes version
+puts "*** 2 bytes version ***"
 start = Time.now
 1000.times do
   talk('h*', 2)
   talk('l*', 2)
 end
 finish = Time.now
-puts "#{(finish - start)} fps"
+puts "#{1000 / (finish - start)} fps"
+
+puts ""
 
 sleep(1)
 
@@ -33,17 +36,22 @@ def padding(s, packetsize)
   s + ("\000" * (packetsize - s.size))
 end
 
-p on = padding('h*', 62)
-p off = padding('l*', 62)
+on = padding('h*', 62)
+off = padding('l*', 62)
 
-# 62 bytes version
+puts "*** #{on.size} bytes version ***"
 start = Time.now
 1000.times do
   talk(on, 2)
   talk(off, 2)
 end
 finish = Time.now
-puts "#{(finish - start)} fps"
+puts "#{1000 / (finish - start)} fps"
 
 # 38400bps = 3840cps
 # 64 / 3840 = 16ms
+# 1000 / (16 + 16) = 32 fps
+
+# 115000bps = 11500cps
+# 64 / 11500 = 6ms
+# 1000 / (6 + 6) = 83 fps
