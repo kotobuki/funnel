abc9970552473b05d421c20e092cceec79d7d5f2
kotobuki <kotobuki@f8328ec6-752f-0410-b566-9d1b0944c958>
2007-09-09 07:09:25 +0000

    * implemented update() for Funnel * fixed bugs... * added test codes for Gainer and Arduino

--- trunk/ruby/funnel.rb
+++ trunk/ruby/funnel.rb
@@ -12,6 +12,21 @@ module Funnel
   class Funnel
     MINIMUM_SAMPLING_INTERVAL = 10
 
+    attr_accessor :auto_update
+
+    def port_def_to_str(type)
+      case type
+      when PORT_AIN
+        return "analog input"
+      when PORT_DIN
+        return "digital input"
+      when PORT_AOUT
+        return "analog output (PWM)"
+      when PORT_DOUT
+        return "digital output"
+      end
+    end
+
     def initialize(host, port, config, interval)
       begin
         @command_port = TCPSocket.open(host, port)
@@ -26,7 +41,7 @@ module Funnel
         raise RuntimeError, "can't connect to the notification port (#{port + 1}) of the funnel_server"
       end
 
-      send_command(OSC::Message.new('/reset'))
+      send_command(OSC::Message.new('/reset'), 5)
       send_command(OSC::Message.new('/configure', 'i' * config.size, *config))
       @port = []
       @port_count = 0
@@ -39,6 +54,7 @@ module Funnel
       @sampling_interval = interval
 
       @auto_update = true
+      @updated_port_indices = []
 
       Thread.new do
         loop do
@@ -63,13 +79,23 @@ module Funnel
       send_command(OSC::Message.new('/polling', 'i', 1))
     end
 
-    def send_command(command)
+    def send_command(command, seconds_to_wait = 1)
       @command_port.send(command.encode, 0)
       packet = nil
       begin
-        timeout(1) {packet = @command_port.recv(4096)}
+        timeout(seconds_to_wait) {packet = @command_port.recv(4096)}
         OSC::Packet.decode(packet).each do |time, message|
-          puts "received: #{message.address}, #{message.to_a}"
+          # puts "received: #{message.address}, #{message.to_a}"
+          if message.to_a[0] < ErrorEvent::NO_ERROR then
+            case message.to_a[0]
+            when ErrorEvent::ERROR:
+              puts "ERROR: #{message.to_a[1]}"
+            when ErrorEvent::REBOOT_ERROR:
+              raise REBOOT_ERROR, "REBOOT_ERROR: #{message.to_a[1]}"
+            when ErrorEvent::CONFIGURATION_ERROR:
+              raise RuntimeError, "CONFIGURATION_ERROR: #{message.to_a[1]}"
+            end
+          end
         end
       rescue TimeoutError
         puts "TimeoutError: command = #{command.address}"
@@ -81,16 +107,16 @@ module Funnel
     def init_ports(config)
       @port.clear
       config.each do |type|
-        puts "port(#{@port_count}): #{type}"
+        puts "port(#{@port_count}): #{port_def_to_str(type)}"
         port = Port.new(@port_count, type)
         @port.push(port)
         if port.direction == PortDirection::OUTPUT then
           port.add_event_listener(Event::CHANGE) do |event|
-            puts "changed(#{event.from}): #{event.value}"
+            # puts "changed(#{event.from}): #{event.value}"
             if (@auto_update) then
-              command = OSC::Message.new('/out', 'if', event.from, event.value)
-              send_command(command)
+              send_output_command(event.from, event.value)
             else
+              @updated_port_indices << event.from
             end
           end
         end
@@ -105,6 +131,31 @@ module Funnel
         return nil
       end
       @port[number]
+    end
+
+    def send_output_command(start, values)
+      command = OSC::Message.new('/out', 'if', start, *values)
+      send_command(command)
+    end
+
+    def update()
+      @updated_port_indices.sort!
+      index = 0
+      start = 0
+      output_values = []
+
+      @updated_port_indices.each_with_index do |number, i|
+        is_not_continuous = @updated_port_indices[i] - index != 1
+        index = @updated_port_indices[i]
+        if is_not_continuous then
+          send_output_command(start, output_values) unless i == 0
+          start = index
+          output_values = []
+        end
+        output_values.push(@port[index].value)
+      end
+      send_output_command(start, output_values)
+      @updated_port_indices = []
     end
 
   end
