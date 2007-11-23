cca16d4ba89b0d3e0a39360c0d10ff85bacddd3a
kotobuki <kotobuki@f8328ec6-752f-0410-b566-9d1b0944c958>
2007-11-23 04:48:17 +0000

    added examples created for the MITOU final presentation

---
+++ trunk/libraries/ruby/fio_demo_1.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require "funnel/fio"
+
+# communicate with a specific node to get sensor values
+module Funnel
+  fio = Fio.new('localhost', 9000)
+  fio.register_node(2)
+
+  fio.iomodule(2).port(0).on PortEvent::CHANGE do |event|
+    s = sprintf("AD0: %5.3f %s", event.target.value, "*" * (event.target.value * 40))
+    puts s
+  end
+
+  sleep(30)
+end
