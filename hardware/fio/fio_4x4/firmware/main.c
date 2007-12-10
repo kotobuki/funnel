189a2ca59baf97ac4266ca65a51f9657a9644e17
Shigeru Kobayashi <kotobuki@users.noreply.github.com>
2007-12-10 03:24:41 +0000

    * modified to match the pin layout * added a note about uartint.asm

--- hardware/fio/fio_4x4/firmware/main.c
+++ hardware/fio/fio_4x4/firmware/main.c
@@ -5,6 +5,20 @@
 #include <m8c.h>        // part specific constants and macros
 #include "PSoCAPI.h"    // PSoC API definitions for all User Modules
 
+/*
+ * NOTE: Be sure to add "ljmp _MY_UART_RX_ISR" to uartint.asm as follows
+   ;@PSoC_UserCode_BODY_2@ (Do not change this line.)
+   ;---------------------------------------------------
+   ; Insert your custom code below this banner
+   ;---------------------------------------------------
+   ;   NOTE: interrupt service routines must preserve
+   ;   the values of the A and X CPU registers.
+   ljmp _MY_UART_RX_ISR
+   ;---------------------------------------------------
+   ; Insert your custom code above this banner
+   ;---------------------------------------------------
+   ;@PSoC_UserCode_END@ (Do not change this line.)
+ */
 #pragma interrupt_handler MY_UART_RX_ISR
 
 BOOL updated = FALSE;
@@ -58,11 +72,11 @@ void main()
 	UART_IntCntl(UART_ENABLE_RX_INT);     // Enable RX interrupts  
 	UART_Start(UART_PARITY_NONE);         // Enable UART  
 
-	PWM8_0_WritePulseWidth(255);
+	PWM8_0_WritePulseWidth(0);
 	PWM8_0_Start();
-	PWM8_1_WritePulseWidth(127);
+	PWM8_1_WritePulseWidth(0);
 	PWM8_1_Start();
-	PWM8_2_WritePulseWidth(63);
+	PWM8_2_WritePulseWidth(0);
 	PWM8_2_Start();
 	PWM8_3_WritePulseWidth(0);
 	PWM8_3_Start();
@@ -71,10 +85,10 @@ void main()
 
 	while(1) {
 		if (updated) {
-			PWM8_0_WritePulseWidth(analogData[10]);
+			PWM8_3_WritePulseWidth(analogData[10]);
 			PWM8_1_WritePulseWidth(analogData[11]);
-			PWM8_2_WritePulseWidth(analogData[12]);
-			PWM8_3_WritePulseWidth(analogData[13]);
+			PWM8_0_WritePulseWidth(analogData[12]);
+			PWM8_2_WritePulseWidth(analogData[13]);
 			updated = FALSE;
 		}
 	}
