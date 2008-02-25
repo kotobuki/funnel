8525003d759551832f222213fccd55e7c5c74a50
kotobuki <kotobuki@f8328ec6-752f-0410-b566-9d1b0944c958>
2008-02-25 07:22:58 +0000

    Implemented Firmata like protocol for testing * Only valid for 9th port (i.e. LED) * Ignores checksum bytes for testing

--- trunk/hardware/fio/fio_8x8/firmware/xbee.c
+++ trunk/hardware/fio/fio_8x8/firmware/xbee.c
@@ -1,6 +1,7 @@
 #include "PSoCAPI.h"    // PSoC API definitions for all User Modules
 
 #include "xbee.h"
+#include "fio.h"
 
 #pragma interrupt_handler UART_RX_ISR
 #pragma interrupt_handler UART_TX_ISR
@@ -9,6 +10,7 @@
 #define ESCAPE			(0x7D)
 #define XON				(0x11)
 #define XOFF			(0x13)
+#define RX_PACKET_16BIT		(0x81)
 #define RX_IO_STATUS_16BIT	(0x83)
 #define AT_COMMAND_RESPONSE	(0x88)
 #define TX_STATUS_MESSAGE	(0x89)
@@ -20,6 +22,7 @@ const BYTE IDX_SOURCE_ADDRESS_MSB = 4;
 const BYTE IDX_SOURCE_ADDRESS_LSB = 5;
 const BYTE IDX_RSSI = 6;
 const BYTE IDX_SAMPLES = 8;
+const BYTE IDX_RF_DATA = 8;
 const BYTE IDX_IO_ENABLE_MSB = 9;
 const BYTE IDX_IO_ENABLE_LSB = 10;
 const BYTE IDX_IO_STATUS_START = 11;
@@ -33,6 +36,39 @@ BYTE rxIndex = 0;
 BYTE rxData[100];
 BYTE rxBytesToReceive = 0;
 WORD rxSum = 0;
+
+// total number of pins currently supported
+#define TOTAL_ANALOG_PINS		(8)
+#define TOTAL_DIGITAL_PINS		(10)
+
+// for comparing along with INPUT and OUTPUT
+#define IN		(0)
+#define OUT		(1)
+#define PWM		(2)
+#define SERVO	(3)
+
+// max number of data bytes in non-SysEx messages
+#define MAX_DATA_BYTES	(2)
+
+#define DIGITAL_MESSAGE			(0x90) // send data for a digital pin
+#define ANALOG_MESSAGE			(0xE0) // send data for an analog pin (or PWM)
+#define REPORT_ANALOG_PIN		(0xC0) // enable analog input by pin #
+#define REPORT_DIGITAL_PORTS	(0xD0) // enable digital input by port pair
+#define START_SYSEX				(0xF0) // start a MIDI SysEx message
+#define SET_DIGITAL_PIN_MODE	(0xF4) // set a digital pin to INPUT or OUTPUT 
+#define END_SYSEX				(0xF7) // end a MIDI SysEx message
+#define REPORT_VERSION			(0xF9) // report firmware version
+#define SYSTEM_RESET			(0xFF) // reset from MIDI
+
+
+// variables for Firmata like message parser
+unsigned char bytesToReceive = 0;
+unsigned char storedInputData[3];
+unsigned char analogData[14];
+
+unsigned char executeMultiByteCommand = 0;
+unsigned char multiByteChannel = 0;
+
 
 BOOL hasPacketToHandle = FALSE;
 
@@ -94,16 +130,14 @@ void UART_RX_ISR()
 			// [START][LENGTH MSB][LENGTH LSB][FRAME DATA][CHECKSUM]
 			rxBytesToReceive = (rxData[1] << 8) + rxData[2] + 4;
 		} else if (rxIndex == (rxBytesToReceive - 1)) {
-			if ((rxSum & 0xFF) + rxData[rxBytesToReceive - 1] == 0xFF) {
+//			if ((rxSum & 0xFF) + rxData[rxBytesToReceive - 1] == 0xFF) {
+			if (1) {
 				hasPacketToHandle = TRUE;
 			}
 		} else if (rxIndex > 2) {
 			rxSum += rxData[rxIndex];
 		}
 	}
-
-	// just for testing
-	hasPacketToHandle = TRUE;
 }
 
 // just to inhibit "multiple define: '_UART_RX_ISR'" error
@@ -113,7 +147,20 @@ void UART_TX_ISR()
 }
 
 void ParsePacket(void) {
+	BYTE length = 0;
+	BYTE i = 0;
+
 	switch ((BYTE)rxData[IDX_API_IDENTIFIER]) {
+	case RX_PACKET_16BIT:
+		// {0x7E}+{0x00}+{0x**}+{0x81}+{MSB}+{LSB}+{RSSI}+{OPTION}+{RF Data}+{Checksum}
+		// OPTION
+		// bit 1: Address Broadcast
+		// bit 2: PAN Broadcast
+		length = rxData[IDX_LENGTH_LSB] - 5;	// RF Data Length
+		for (i = 0; i < length; i++) {
+			ParseFirmataMessage(rxData[IDX_RF_DATA + i]);
+		}
+		break;
 	case TX_STATUS_MESSAGE:
 		// {0x7E}+{0x00+0x03}+{0x89}+{Frame ID}+{Status}+{Checksum}
 		switch (rxData[IDX_API_IDENTIFIER + 2]) {
@@ -160,6 +207,92 @@ void ParsePacket(void) {
 		break;
 	default:
 		break;
+	}
+}
+
+void ParseFirmataMessage(BYTE inputData) {
+	BYTE command = 0;
+	
+	if (bytesToReceive > 0 & inputData < 128) {
+		bytesToReceive--;
+		storedInputData[bytesToReceive] = inputData;
+		if ((executeMultiByteCommand != 0) && (bytesToReceive == 0)) {
+ 			switch (executeMultiByteCommand) {
+			case ANALOG_MESSAGE:
+				analogData[multiByteChannel] = (BYTE)((storedInputData[0] << 7) + storedInputData[1]);
+				break;
+			case DIGITAL_MESSAGE:
+				SetDigitalOutputs((WORD)((storedInputData[0] << 7) + storedInputData[1])); //(LSB, MSB)
+				break;
+			case SET_DIGITAL_PIN_MODE:
+//				setPinMode(storedInputData[1], storedInputData[0]); // (pin#, mode)
+//				if(storedInputData[0] == INPUT) 
+//				digitalInputsEnabled = true; // enable reporting of digital inputs
+				break;
+			case REPORT_ANALOG_PIN:
+//				setAnalogPinReporting(multiByteChannel,storedInputData[0]);
+				break;
+			case REPORT_DIGITAL_PORTS:
+				// TODO: implement MIDI channel as port base for more than 16 digital inputs
+//				if(storedInputData[0] == 0)
+//					digitalInputsEnabled = false;
+//				else
+//				digitalInputsEnabled = true;
+				break;
+			default:
+				break;
+ 			}
+ 			executeMultiByteCommand = 0;
+ 		}
+	} else {
+		if (inputData < 0xF0) {
+			command = inputData & 0xF0;
+			multiByteChannel = inputData & 0x0F;
+		} else {
+			command = inputData;
+		}
+
+		switch (command) {
+		case ANALOG_MESSAGE:
+		case DIGITAL_MESSAGE:
+		case SET_DIGITAL_PIN_MODE:
+			bytesToReceive = 2;
+			executeMultiByteCommand = command;
+			break;
+		case REPORT_ANALOG_PIN:
+		case REPORT_DIGITAL_PORTS:
+			bytesToReceive = 1; // two data bytes needed
+			executeMultiByteCommand = command;
+			break;
+		case SYSTEM_RESET:
+			// this doesn't do anything yet
+			break;
+		case REPORT_VERSION:
+			// this doesn't do anything yet
+			break;
+		default:
+			break;
+		}
+	}	
+}
+
+void SetDigitalOutputs(WORD newState)
+{
+	BYTE i;
+	WORD mask;
+
+	for (i = 0; i < TOTAL_DIGITAL_PINS; ++i) {
+		mask = 1 << i;
+//		if ((digitalPinStatus & mask) && !(pwmStatus & mask)) {
+//			digitalWrite(i, twoBytesForPorts & mask ? HIGH : LOW);
+//		} 
+		if (i == 9) {
+			if (newState & mask) {
+				SET_LED_H();
+			} else {
+				SET_LED_L();
+			}
+		}
 	}
 }
 
--- trunk/hardware/fio/fio_8x8/firmware/xbee.h
+++ trunk/hardware/fio/fio_8x8/firmware/xbee.h
@@ -4,6 +4,10 @@ BOOL HasPacketToHandle(void);
 void ClearHasPacketFlag(void);
 void ParsePacket(void);
 
+// Firmata like API related functions
+void ParseFirmataMessage(BYTE inputData);
+void SetDigitalOutputs(WORD newState);
+
 void ReportIOStatus(WORD ioEnable, WORD dioStatus, WORD *adcStatus, BYTE adcChannels);
 void SendTransmitRequest(WORD destAddress, BYTE rfDataLength);
 void SendCommand(BYTE frameDataLength);
