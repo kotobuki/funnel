07e2296a2985640decfcc44eaf7d7d04dc930104
kotobuki <kotobuki@f8328ec6-752f-0410-b566-9d1b0944c958>
2008-05-23 02:33:28 +0000

    fixed the deadlock issue (actually an array was not initialized)

--- trunk/server/src/funnel/FunnelIO.java
+++ trunk/server/src/funnel/FunnelIO.java
@@ -14,16 +14,13 @@ public class FunnelIO extends FirmataIO implements XBeeEventListener {
 	private static final int[] PWM_CAPABLE_PINS = new int[] { 8, 9, 10, 11, 12,
 			13, 14, 15 };
 
-	private static final int MAX_IO_PORT = 9;
+	private static final int MAX_IO_PORT = 18;
 	private static final int MAX_NODES = 65535;
 	private float[][] inputData = new float[MAX_NODES][MAX_IO_PORT];
 	private int[] rssi = new int[MAX_NODES];
 	private Hashtable nodes;
 
 	private XBee xbee;
-	private boolean hasPacket = false;
-	private int source = 0;
-	private int[] rxData;
 
 	public FunnelIO(FunnelServer server, String serialPortName, int baudRate) {
 		super(TOTAL_ANALOG_PINS, TOTAL_DIGITAL_PINS, PWM_CAPABLE_PINS);
@@ -42,7 +39,6 @@ public class FunnelIO extends FirmataIO implements XBeeEventListener {
 
 	void writeByte(int data) {
 		// TODO Auto-generated method stub
-
 	}
 
 	public void firmwareVersionEvent(String version) {
@@ -104,19 +100,15 @@ public class FunnelIO extends FirmataIO implements XBeeEventListener {
 	}
 
 	public void rxPacketEvent(int source, int rssi, int options, int[] rxData) {
-		this.source = source;
 		this.rssi[source] = rssi;
-		// parent.printMessage(new String("src:" + source + " , rssi:" + rssi
-		// + ",data:" + rxData.length));
-		// parent.printMessage(new String("ain 0: "));
-		// for (int i = 0; i < rxData.length; i++) {
-		// processInput(rxData[i]);
-		// }
-		this.rxData = new int[rxData.length];
-		for (int i = 0; i < this.rxData.length; i++) {
-			this.rxData[i] = rxData[i];
+		for (int i = 0; i < rxData.length; i++) {
+			processInput(rxData[i]);
 		}
-		hasPacket = true;
+		for (int i = 0; i < TOTAL_ANALOG_PINS; i++) {
+			this.inputData[source][i] = this.analogData[i];
+		}
+		printMessage(new String("ain: " + this.inputData[source][0] + ","
+				+ this.inputData[source][1]));
 	}
 
 	public void sourceAddressEvent(String sourceAddress) {
@@ -158,13 +150,6 @@ public class FunnelIO extends FirmataIO implements XBeeEventListener {
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-
-		if (hasPacket) {
-			for (int i = 0; i < this.rxData.length; i++) {
-				processInput(this.rxData[i]);
-			}
-			hasPacket = false;
 		}
 	}
 
