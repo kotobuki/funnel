7931983adf03f505b9426fd8b829ab615ae8bd40
kotobuki <kotobuki@f8328ec6-752f-0410-b566-9d1b0944c958>
2008-07-08 14:22:46 +0000

    Modified to do not send a duplicate reply to a server command on exceptions

--- trunk/server/src/funnel/FunnelServer.java
+++ trunk/server/src/funnel/FunnelServer.java
@@ -26,7 +26,7 @@ public class FunnelServer extends Frame {
 	 */
 	private static final long serialVersionUID = -2518876146630199843L;
 
-	private static final String buildName = "Funnel 008 (2008-07-07) [EXPERIMENTAL]";
+	private static final String buildName = "Funnel 008 (2008-07-08) [EXPERIMENTAL]";
 
 	private CommandPortServer server;
 	private IOModule ioModule = null;
