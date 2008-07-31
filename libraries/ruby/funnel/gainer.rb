a6bbfee35b28e7278ed15aa3790dd4e8a055342d
Shigeru Kobayashi <kotobuki@users.noreply.github.com>
2008-07-31 13:41:33 +0000

    * updated configurations * renamed XBee::XBS1 and XBee::XBS2 to XBee::MULTIPOINT and XBee::ZNET * added following methods to IOModule ** analog_input ** digital_input ** analog_output ** diigital_output ** analog_pin ** digital_pin * moved digital pins formwer about Arduino and Fio

--- libraries/ruby/funnel/gainer.rb
+++ libraries/ruby/funnel/gainer.rb
@@ -52,61 +52,70 @@ module Funnel
         interval = arguments[:interval] unless arguments[:interval] == nil
         applet = arguments[:applet]
       end
-      
+
       config = Configuration.new(Configuration::GAINER, mode)
       @system = IOSystem.new(config, host, port, interval, applet)
 
-      @ain_ports = config.ain_ports
-      @din_ports = config.din_ports
-      @aout_ports = config.aout_ports
-      @dout_ports = config.dout_ports
-      @button = config.button
-      @led = config.led
+      # @ain_ports = config.ain_ports
+      # @din_ports = config.din_ports
+      # @aout_ports = config.aout_ports
+      # @dout_ports = config.dout_ports
+      # @button = config.button
+      # @led = config.led
 
+      # set SetPoint filters to each digital inputs
       button.filters = [SetPoint.new(0.5, 0)] unless @button == nil
+      config.din_ports.each do |i|
+        @system.io_module(0).port(i).filters = [SetPoint.new(0.5, 0)]
+      end
     end
 
     def analog_input(number)
-      return if @ain_ports == nil
-      raise ArguentError, "analog input is not availabe at #{number}" if @ain_ports.at(number) == nil
-      @system.io_module(0).port(@ain_ports.at(number))
+      return @system.io_module(0).analog_input number
+      # return if @ain_ports == nil
+      # raise ArguentError, "analog input is not availabe at #{number}" if @ain_ports.at(number) == nil
+      # @system.io_module(0).port(@ain_ports.at(number))
     end
 
     def digital_input(number)
-      return if @din_ports == nil
-      raise ArguentError, "digital input is not availabe at #{number}" if @din_ports.at(number) == nil
-      @system.io_module(0).port(@din_ports.at(number))
+      return @system.io_module(0).digital_input number
+      # return if @din_ports == nil
+      # raise ArguentError, "digital input is not availabe at #{number}" if @din_ports.at(number) == nil
+      # @system.io_module(0).port(@din_ports.at(number))
     end
 
     def analog_output(number)
-      return if @aout_ports == nil
-      raise ArguentError, "analog output is not availabe at #{number}" if @aout_ports.at(number) == nil
-      @system.io_module(0).port(@aout_ports.at(number))
+      return @system.io_module(0).analog_output number
+      # return if @aout_ports == nil
+      # raise ArguentError, "analog output is not availabe at #{number}" if @aout_ports.at(number) == nil
+      # @system.io_module(0).port(@aout_ports.at(number))
     end
 
     def digital_output(number)
-      return if @dout_ports == nil
-      raise ArguentError, "digital output is not availabe at #{number}" if @dout_ports.at(number) == nil
-      @system.io_module(0).port(@dout_ports.at(number))
+      return @system.io_module(0).digital_output number
+      # return if @dout_ports == nil
+      # raise ArguentError, "digital output is not availabe at #{number}" if @dout_ports.at(number) == nil
+      # @system.io_module(0).port(@dout_ports.at(number))
     end
 
     def button(number = 0)
-      return if @button == nil
-      raise ArguentError, "button is not availabe at #{number}" if @button.at(number) == nil
-      @system.io_module(0).port(@button.at(number))
+      return @system.io_module(0).button number
+      # return if @button == nil
+      # raise ArguentError, "button is not availabe at #{number}" if @button.at(number) == nil
+      # @system.io_module(0).port(@button.at(number))
     end
 
     def led(number = 0)
-      return if @led == nil
-      raise ArguentError, "LED is not availabe at #{number}" if @led.at(number) == nil
-      @system.io_module(0).port(@led.at(number))
+      return @system.io_module(0).led number
+      # return if @led == nil
+      # raise ArguentError, "LED is not availabe at #{number}" if @led.at(number) == nil
+      # @system.io_module(0).port(@led.at(number))
     end
 
     alias :ain :analog_input
     alias :din :digital_input
     alias :aout :analog_output
     alias :dout :digital_output
-
   end
 
 end
