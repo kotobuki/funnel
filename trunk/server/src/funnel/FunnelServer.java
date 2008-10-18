329865d5919f5467d731dcde954f27d8ff023636
kotobuki <kotobuki@f8328ec6-752f-0410-b566-9d1b0944c958>
2008-10-18 13:44:38 +0000

    * modified to fix issue #19 * fixed a bug in sending pin mode commands about AIN and PWM (swapped)

--- trunk/server/src/funnel/FunnelServer.java
+++ trunk/server/src/funnel/FunnelServer.java
@@ -26,7 +26,7 @@ public class FunnelServer extends Frame {
 	 */
 	private static final long serialVersionUID = -2518876146630199843L;
 
-	private static final String buildName = "Funnel 008 BETA (r495)";
+	private static final String buildName = "Funnel 008 BETA (r496)";
 
 	private CommandPortServer server;
 	private IOModule ioModule = null;
