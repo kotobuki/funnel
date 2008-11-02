da9bee2918f68cd04946a2d5dec0bc50322c339e
Shigeru Kobayashi <kotobuki@users.noreply.github.com>
2008-11-02 11:46:58 +0000

    added I2CDevice and HMC6352 to support I2C devices per IOModule

--- libraries/ruby/examples/arduino_test.rb
+++ libraries/ruby/examples/arduino_test.rb
@@ -30,24 +30,40 @@ config.set_digital_pin_mode(12, IN)
 config.set_digital_pin_mode(13, OUT)
 aio = Arduino.new :config => config
 
-aio.a(0).on CHANGE do |event|
-  puts "A0: #{event.target.last_value} > #{event.target.value}"
+# aio.a(0).on CHANGE do |event|
+#   puts "A0: #{event.target.last_value} > #{event.target.value}"
+# end
+# 
+# aio.d(12).on CHANGE do |event|
+#   puts "D12: #{event.target.last_value} > #{event.target.value}"
+# end
+# 
+# Osc.service_interval = 20
+# blinker = Osc.new Osc::SQUARE, 2.0, 0
+# aio.d(13).filters = [blinker]
+# blinker.reset
+# blinker.start
+# 
+# fader = Osc.new(Osc::SIN, 1.0, 0)
+# scaler = Scaler.new 0.0, 1.0, 0.0, 1.0, Scaler::SQUARE
+# aio.d(11).filters = [fader, scaler]
+# fader.reset
+# fader.start
+
+# aio.send_sysex 0x76, [0, 0x21, 0x47, 0x74, 0x51] #write, 'G'
+# aio.send_sysex 0x76, [0, 0x21, 0x41] #write, 'A'
+# 
+# 100.times do
+#   aio.send_sysex 0x76, [1, 0x21, 0x7F, 0x02] #read
+#   sleep 0.1
+# end
+
+compass = HMC6352.new aio
+
+10.times do
+  compass.update
+  sleep 0.5
+  puts "heading: #{compass.heading}"
 end
 
-aio.d(12).on CHANGE do |event|
-  puts "D12: #{event.target.last_value} > #{event.target.value}"
-end
-
-Osc.service_interval = 20
-blinker = Osc.new Osc::SQUARE, 2.0, 0
-aio.d(13).filters = [blinker]
-blinker.reset
-blinker.start
-
-fader = Osc.new(Osc::SIN, 1.0, 0)
-scaler = Scaler.new 0.0, 1.0, 0.0, 1.0, Scaler::SQUARE
-aio.d(11).filters = [fader, scaler]
-fader.reset
-fader.start
-
-sleep(10)
+sleep 1
--- libraries/ruby/funnel/iosystem.rb
+++ libraries/ruby/funnel/iosystem.rb
@@ -103,7 +103,12 @@ module Funnel
                 when '/out'
                   puts "ERROR: #{message.address}" if message.to_a[0] < FunnelErrorEvent::NO_ERROR
                 when '/sysex'
-                  p message.to_a
+                  case message.to_a[1]
+                  when 0x76
+                    @modules[message.to_a[0]].handle_sysex(message.to_a[2..(message.to_a.size - 1)]) unless @modules[message.to_a[0]] == nil
+                  when 0x71
+                    puts "Firmata String (at #{message.to_a[0]}): #{message.to_a[2]}"
+                  end
                 end
               end
             rescue EOFError
