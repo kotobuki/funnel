97880f5c09e0554c486d319b820f2203d5946ce7
kotobuki <kotobuki@f8328ec6-752f-0410-b566-9d1b0944c958>
2009-04-08 04:49:57 +0000

    modified to support cross-domain policy issue for recent releases of Flash Player

--- trunk/server/src/funnel/CommandPortClient.java
+++ trunk/server/src/funnel/CommandPortClient.java
@@ -3,6 +3,7 @@ package funnel;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.Arrays;
 
 import com.illposed.osc.OSCListener;
 import com.illposed.osc.OSCMessage;
@@ -47,6 +48,9 @@ public class CommandPortClient extends Client implements Runnable {
 	public final int REBOOT_ERROR = -2;
 	public final int CONFIGURATION_ERROR = -3;
 
+	private final String fpQueryString = "<policy-file-request/>\0";
+	private final String fpReplyString = "<cross-domain-policy><allow-access-from domain=\"*\" to-ports=\"*\"/></cross-domain-policy>\0";
+
 	/**
 	 * Create an OSCPort that listens on the specified port.
 	 * 
@@ -78,7 +82,8 @@ public class CommandPortClient extends Client implements Runnable {
 				server.getIOModule().reboot();
 				sendSimpleReply(message.getAddress(), NO_ERROR, null);
 			} catch (Exception e) {
-				sendSimpleReply(message.getAddress(), REBOOT_ERROR, e.getMessage());
+				sendSimpleReply(message.getAddress(), REBOOT_ERROR, e
+						.getMessage());
 			}
 		} else if (message.getAddress().equals("/polling")) {
 			try {
@@ -89,8 +94,8 @@ public class CommandPortClient extends Client implements Runnable {
 			}
 		} else if (message.getAddress().startsWith("/in")) {
 			try {
-				Object[] reply = server.getIOModule().getInputs(message.getAddress(),
-						message.getArguments());
+				Object[] reply = server.getIOModule().getInputs(
+						message.getAddress(), message.getArguments());
 				sendReply(message.getAddress(), reply);
 			} catch (IllegalArgumentException e) {
 				sendSimpleReply(message.getAddress(), ERROR, e.getMessage());
@@ -111,11 +116,13 @@ public class CommandPortClient extends Client implements Runnable {
 				server.getIOModule().setConfiguration(message.getArguments());
 				sendSimpleReply(message.getAddress(), NO_ERROR, null);
 			} catch (IllegalArgumentException e) {
-				sendSimpleReply(message.getAddress(), CONFIGURATION_ERROR, e.getMessage());
+				sendSimpleReply(message.getAddress(), CONFIGURATION_ERROR, e
+						.getMessage());
 			}
 		} else if (message.getAddress().equals("/sysex/request")) {
 			try {
-				server.getIOModule().sendSystemExclusiveMessage(message.getArguments());
+				server.getIOModule().sendSystemExclusiveMessage(
+						message.getArguments());
 				sendSimpleReply(message.getAddress(), NO_ERROR, null);
 			} catch (Exception e) {
 				sendSimpleReply(message.getAddress(), ERROR, e.getMessage());
@@ -159,14 +166,26 @@ public class CommandPortClient extends Client implements Runnable {
 							+ (buffer[processedSize + 3] & 0xFF);
 
 					if (packetSize > 1536) {
-						server
-								.printMessage("ERROR: Your client's endianness is not compatible with the server.");
+						if (processedSize == 0) {
+							// if this is a first packet, it might be a cross
+							// domain query from a FlashPlayer
+							String s = new String(buffer, 0, readSize);
+							if (s.equals(fpQueryString)) {
+								bufferedOut.write(fpReplyString.getBytes());
+								bufferedOut.flush();
+							}
+						} else {
+							server
+									.printMessage("ERROR: Your client's endianness is not compatible with the server.");
+							System.out.println(Arrays.toString(buffer));
+						}
 						break;
 					}
 
 					// TODO: Modify here to do not copy arrays to improve
 					// performance
-					System.arraycopy(buffer, processedSize + 4, packet, 0, packetSize);
+					System.arraycopy(buffer, processedSize + 4, packet, 0,
+							packetSize);
 					OSCPacket oscPacket = converter.convert(packet, packetSize);
 					dispatcher.dispatchPacket(oscPacket);
 					processedSize += packetSize + 4;
@@ -176,7 +195,8 @@ public class CommandPortClient extends Client implements Runnable {
 			e.printStackTrace();
 		} finally {
 			server
-					.printMessage(Messages.getString("FunnelServer.CommandPort") + getIP() + Messages.getString("Server.ClientDisconnected")); //$NON-NLS-1$
+					.printMessage(Messages
+							.getString("FunnelServer.CommandPort") + getIP() + Messages.getString("Server.ClientDisconnected")); //$NON-NLS-1$
 			server.getIOModule().reboot();
 			close();
 		}
@@ -194,7 +214,9 @@ public class CommandPortClient extends Client implements Runnable {
 		try {
 			send(reply);
 		} catch (Exception e) {
-			server.printMessage("Error sending reply to the client: " + address);
+			server
+					.printMessage("Error sending reply to the client: "
+							+ address);
 		}
 	}
 
@@ -203,7 +225,9 @@ public class CommandPortClient extends Client implements Runnable {
 		try {
 			send(reply);
 		} catch (Exception e) {
-			server.printMessage("Error sending reply to the client: " + address);
+			server
+					.printMessage("Error sending reply to the client: "
+							+ address);
 		}
 	}
 
