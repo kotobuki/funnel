ad38f612b7d67c13661195e357aad7358051db05
kotobuki <kotobuki@f8328ec6-752f-0410-b566-9d1b0944c958>
2009-04-20 03:17:46 +0000

    updated for Funnel 009 RC1

--- trunk/release.sh
+++ trunk/release.sh
@@ -1,7 +1,13 @@
-cp ./server/funnel_server.jar ./libraries/processing/library
+#!/bin/sh
+mv ./server/funnel_server.jar ./libraries/processing/library
 cp ./server/rxtxSerial.dll ./libraries/processing/library
 cp ./server/librxtxSerial.* ./libraries/processing/library
 cp ./server/settings.*.txt ./libraries/processing/library
 cp ./server/lib/RXTXcomm.jar ./libraries/processing/library
 cp ./server/lib/jvyaml.jar ./libraries/processing/library
 cp ./documents/src/installation_instructions_*.pdf ./documents
+rm -r ./documents/src/
+rm -r ./release/
+mv ./server/rxtxSerial.dll ./server/lib
+rm ./server/librxtxSerial.*
+rm ./release.sh
