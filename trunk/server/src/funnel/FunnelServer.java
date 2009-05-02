6879ad103ffb41ec8bd1e5dd4aad5019376b0c24
kotobuki <kotobuki@f8328ec6-752f-0410-b566-9d1b0944c958>
2009-05-02 02:00:36 +0000

    modified to stop polling on each reboot

--- trunk/server/src/funnel/FunnelServer.java
+++ trunk/server/src/funnel/FunnelServer.java
@@ -26,7 +26,7 @@ public class FunnelServer extends Frame {
 	 */
 	private static final long serialVersionUID = -2518876146630199843L;
 
-	private static final String buildName = "Funnel 009 BETA (r588)";
+	private static final String buildName = "Funnel Server (r626)";
 
 	private CommandPortServer server;
 	private IOModule ioModule = null;
