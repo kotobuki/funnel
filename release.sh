00604224f64140536375b0fd9fac7f6065a16a4c
Shigeru Kobayashi <kotobuki@users.noreply.github.com>
2009-07-26 07:23:54 +0000

    modified to create archives of Funnel Server for physical x wonderfl

--- release.sh
+++ release.sh
@@ -10,5 +10,27 @@ rm -r ./documents/src/
 rm -r ./release/
 mv ./server/rxtxSerial.dll ./server/lib
 rm ./server/librxtxSerial.*
-rm ./release.sh
 rm -r ./libraries/of/
+mkdir ./funnel_server-mac
+mkdir ./funnel_server-mac/Arduino
+mkdir ./funnel_server-mac/Gainer
+mkdir ./funnel_server-win
+mkdir ./funnel_server-win/Arduino
+mkdir ./funnel_server-win/Gainer
+cp -r ./server/Funnel\ Server.app ./funnel_server-mac/Gainer/
+cp ./server/settings.gainer.txt ./funnel_server-mac/Gainer/settings.txt
+cp -r ./server/Funnel\ Server.app ./funnel_server-mac/Arduino/
+cp ./server/settings.arduino.txt ./funnel_server-mac/Arduino/settings.txt
+cp -r ./server/Funnel\ Server.exe ./funnel_server-win/Gainer/
+cp ./server/settings.gainer.txt ./funnel_server-win/Gainer/settings.txt
+cp -r ./server/lib ./funnel_server-win/Gainer/
+cp -r ./server/Funnel\ Server.exe ./funnel_server-win/Arduino/
+cp ./server/settings.arduino.txt ./funnel_server-win/Arduino/settings.txt
+cp -r ./server/lib ./funnel_server-win/Arduino/
+cp ./*.txt ./funnel_server-mac/
+cp ./*.txt ./funnel_server-win/
+hdiutil create -format UDRO -fs HFS+ -srcfolder ./funnel_server-mac -volname "funnel_server" funnel_server-mac.dmg
+zip -r funnel_server-win.zip ./funnel_server-win/
+rm -r ./funnel_server-mac/
+rm -r ./funnel_server-win/
+rm ./release.sh
