41074e76576ad1f1db3f8425a0fc5712f013008f
kotobuki <kotobuki@f8328ec6-752f-0410-b566-9d1b0944c958>
2009-10-17 05:24:42 +0000

    fixed a bug about the SERVO_CONFIG system exclusive message of Firmata

--- trunk/server/src/funnel/FunnelServer.java
+++ trunk/server/src/funnel/FunnelServer.java
@@ -26,7 +26,7 @@ public class FunnelServer extends Frame {
 	 */
 	private static final long serialVersionUID = -2518876146630199843L;
 
-	private static final String buildName = "Funnel Server 010 (r676) [BETA]";
+	private static final String buildName = "Funnel Server 010 (r698) [BETA]";
 
 	private CommandPortServer server;
 	private IOModule ioModule = null;
