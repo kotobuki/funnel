09f179e72aa0ccaffc45289d8f5755744d19ecc0
kotobuki <kotobuki@f8328ec6-752f-0410-b566-9d1b0944c958>
2010-05-27 14:42:46 +0000

    * added README_Japanino_ja.txt * modified release.sh to support distributions for Japanino * updated documents for the AS3 library * updated examples/AruinoTest.* * updated the board layout and added PDF filed for the LCD shield * added JapaninoPOVCompass

--- trunk/release.sh
+++ trunk/release.sh
@@ -18,12 +18,20 @@ cp -r ./server/Funnel\ Server.exe ./funnel_server-win/
 cp -r ./server/lib ./funnel_server-win/
 cp ./*.txt ./funnel_server-mac/
 cp ./*.txt ./funnel_server-win/
+cp -r ./hardware/japanino/ ./funnel_server-mac/Japanino
+cp -r ./hardware/japanino/ ./funnel_server-win/Japanino
 mkdir ./tools
 cp -r ./hardware/fio/tool/ ./tools/
 hdiutil create -format UDRO -fs HFS+ -srcfolder ./funnel_server-mac -volname "funnel_server" funnel_server-mac.dmg
 zip -r funnel_server-win.zip ./funnel_server-win/
 rm -r ./funnel_server-mac/
 rm -r ./funnel_server-win/
-cp ./funnel_server-win.zip ./server/win.zip
-cp ./funnel_server-mac.dmg ./server/mac.dmg
+cp ./funnel_server-win.zip ./server/windows.zip
+cp ./funnel_server-mac.dmg ./server/macosx.dmg
+mv ./funnel_server-win.zip ./funnel_server_japanino.zip
+mv ./funnel_server-mac.dmg ./funnel_server_japanino.dmg
+rm ./server/*.txt
+rm ./server/*.exe
+rm -r ./server/lib/
+rm -r ./server/Funnel\ Server.app/
 rm ./release.sh
